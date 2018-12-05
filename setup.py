265a90a01e9b7ba3d6c8ee7c2552b4a27ba2ba62
santoshphilip <santosh_philip@yahoo.com>
2018-12-04 21:49:08 -0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     include_package_data=True,
     keywords='witheppy',
     name='witheppy',
-    packages=find_packages(include=['witheppy']),
+    packages=find_packages(include=['witheppy', 'witheppy.eppyhelpers']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
