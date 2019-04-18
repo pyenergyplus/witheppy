4eb38b7eb2bc71ca415bdfa8f647aaf4d0cd0250
Santosh Philip <santosh_philip@yahoo.com>
2019-04-18 15:15:57 -0700

    new file:   witheppy/runner.py

---
+++ witheppy/runner.py
@@ -0,0 +1,28 @@
+"""helpers that enhance idf.run"""
+
+import os
+import eppy
+
+def eplaunch_run(idf, weather=None):
+    """run the way eplaunch would run one simulation"""
+    fname = idf.idfname
+    if not weather:
+        wfile = idf.epw
+    else:
+        wfile = weather
+    folder = os.path.dirname(fname)
+    noext = os.path.splitext(os.path.basename(fname))[0]
+    idf.run(expandobjects=True, output_directory=folder, output_prefix=noext)
+
+
+# TODO : Use this function to build a bunch of things that are needed:
+- read idd from disk for testing
+- see how eppy does testing for run
+- then test this function and all the edge conditions for this function.
+
+# --
+fname = "/Users/santosh/Documents/temp/eppyz-run/run2/1ZoneDataCenterCRAC_wPumpedDXCoolingCoil.idf"
+wfile = "/Users/santosh/Documents/temp/eppyz-run/USA_CO_Denver/USA_CO_Denver.Intl.AP.725650_TMY3.epw"
+idf = eppy.openidf(fname, epw=wfile)
+eplaunch_run(idf)
+
