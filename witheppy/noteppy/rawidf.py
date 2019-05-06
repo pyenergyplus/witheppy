b6a4df7a9ceb6d71d1a6aff0b1b9bf9cce0c8c8c
Santosh Philip <santosh_philip@yahoo.com>
2019-05-06 16:50:36 -0700

    modified:   witheppy/noteppy/rawidf.py

--- witheppy/noteppy/rawidf.py
+++ witheppy/noteppy/rawidf.py
@@ -11,6 +11,55 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-def readrawidf(fname):
+def removecomment(astr, cphrase):
+    """
+    the comment is similar to that in python.
+    any charachter after the # is treated as a comment
+    until the end of the line
+    astr is the string to be de-commented
+    cphrase is the comment phrase"""
+    # linesep = mylib3.getlinesep(astr)
+    alist = astr.splitlines()
+    for i in range(len(alist)):
+        alist1 = alist[i].split(cphrase)
+        alist[i] = alist1[0]
+
+    # return string.join(alist, linesep)
+    return '\n'.join(alist)
+
+def readrawidf(fhandle):
     """read the idf file as a dict. Dict keys are idfobjkeys, dict values are list in list"""
-    pass
+    astr = fhandle.read()
+    nocom = removecomment(astr, '!')
+    idfst = nocom
+    scount = 0
+    alist = idfst.split(';')
+    rawidf = {}
+    for element in alist:
+        lst = element.split(',')
+        lst = [item.strip() for item in lst]
+        key = lst[0].strip().upper()
+        if not key:
+            continue
+        rawidf.setdefault(key, [])
+        rawidf[key].append(lst)
+    return rawidf
+
+def rawidf2str(rawidf, order=None):
+    """string rep of rawidf"""
+    lst = []
+    if order:
+        keys = order
+    else:
+        keys = rawidf.keys()
+    for key in keys:
+        for vals in rawidf[key]:
+            lst.append('{},'.format(key))
+            for val in vals[1:]:
+                lst.append('     {},'.format(val))
+            lst.pop(-1)
+            lst.append('    {};'.format(val))
+            lst.append('')
+    return '\n'.join(lst)
+
+
