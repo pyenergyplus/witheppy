f18e6d5d97b6c9dacc9c77d06322a182ae5b8e43
Santosh Philip <santosh_philip@yahoo.com>
2019-05-06 15:05:29 -0700

    new file:   witheppy/noteppy/rawidf.py

---
+++ witheppy/noteppy/rawidf.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2019 Santosh Philip
+# =======================================================================
+#  Distributed under the MIT License.
+#  (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+# =======================================================================
+"""read and save idf files without using eppy"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+def readrawidf(fname):
+    """read the idf file as a dict. Dict keys are idfobjkeys, dict values are list in list"""
+    pass
